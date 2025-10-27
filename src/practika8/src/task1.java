7a854e20039dd53e2f29cecf0776f10cdea1d30d
arttr1 <arttr170bu@gmail.com>
2025-10-27 17:15:44 +0300

    task6 done

--- src/practika8/src/task1.java
+++ src/practika8/src/task1.java
@@ -4,6 +4,7 @@ public class task1 {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
+        input.close();
 
         for (int i = 1, printed = 0, count = 0; printed < n; printed++){
             System.out.print(i);
--- src/practika8/src/task2.java
+++
@@ -1,7 +0,0 @@
-
-
-public class task2{
-    public static void main(String[] args) {
-        
-    }
-}
